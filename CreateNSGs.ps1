88acdc6a111867b1aa59e80a6f39bd12ae3ff7d6
Saurin Shah <saurinsh@microsoft.com>
2017-02-10 05:37:41 -0800

    CreateNSG.ps1

---
+++ CreateNSGs.ps1
@@ -0,0 +1,78 @@
+# Login to Azure
+Login-AzureRmAccount -subscriptionid 450112bd-8022-4a38-ba61-8b101ee13add
+
+$vnetName = "contosohdiarmvnet"
+$resourceGroupName = "contosohdi"
+$subnetName = "hdisubnet"
+
+# Get the Virtual Network object
+$vnet = Get-AzureRmVirtualNetwork `
+    -Name $vnetName `
+    -ResourceGroupName $resourceGroupName
+
+# Get the region the Virtual network is in.
+$location = $vnet.Location
+
+# Get the subnet object
+$subnet = $vnet.Subnets | Where-Object Name -eq $subnetName
+
+# Create a new Network Security Group.
+# And add exemptions for the HDInsight health and management services.
+$nsg = New-AzureRmNetworkSecurityGroup `
+    -Name "hdisecure" `
+    -ResourceGroupName $resourceGroupName `
+    -Location $location `
+    | Add-AzureRmNetworkSecurityRuleConfig `
+        -name "hdirule1" `
+        -Description "HDI health and management address 168.61.49.99" `
+        -Protocol "*" `
+        -SourcePortRange "*" `
+        -DestinationPortRange "443" `
+        -SourceAddressPrefix "168.61.49.99" `
+        -DestinationAddressPrefix "VirtualNetwork" `
+        -Access Allow `
+        -Priority 300 `
+        -Direction Inbound `
+    | Add-AzureRmNetworkSecurityRuleConfig `
+        -Name "hdirule2" `
+        -Description "HDI health and management 23.99.5.239" `
+        -Protocol "*" `
+        -SourcePortRange "*" `
+        -DestinationPortRange "443" `
+        -SourceAddressPrefix "23.99.5.239" `
+        -DestinationAddressPrefix "VirtualNetwork" `
+        -Access Allow `
+        -Priority 301 `
+        -Direction Inbound `
+    | Add-AzureRmNetworkSecurityRuleConfig `
+        -Name "hdirule3" `
+        -Description "HDI health and management 168.61.48.131" `
+        -Protocol "*" `
+        -SourcePortRange "*" `
+        -DestinationPortRange "443" `
+        -SourceAddressPrefix "168.61.48.131" `
+        -DestinationAddressPrefix "VirtualNetwork" `
+        -Access Allow `
+        -Priority 302 `
+        -Direction Inbound `
+    | Add-AzureRmNetworkSecurityRuleConfig `
+        -Name "hdirule4" `
+        -Description "HDI health and management 138.91.141.162" `
+        -Protocol "*" `
+        -SourcePortRange "*" `
+        -DestinationPortRange "443" `
+        -SourceAddressPrefix "138.91.141.162" `
+        -DestinationAddressPrefix "VirtualNetwork" `
+        -Access Allow `
+        -Priority 303 `
+        -Direction Inbound
+
+# Set the changes to the security group
+Set-AzureRmNetworkSecurityGroup -NetworkSecurityGroup $nsg
+
+# Apply the NSG to the subnet
+Set-AzureRmVirtualNetworkSubnetConfig `
+    -VirtualNetwork $vnet `
+    -Name $subnetName `
+    -AddressPrefix $subnet.AddressPrefix `
+    -NetworkSecurityGroup $nsg
